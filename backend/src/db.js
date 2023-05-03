7f7e8f25c99922a1e710fd4c6fa2c6d573868add
Ganesh <masterminds0809@gmail.com>
2023-05-03 15:08:13 +0530

    ok

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,7 +5,7 @@ let db;
 async function connectToDB(cb){
     const url = "mongodb+srv://bora1132004:Ganesh@cluster0.a93zhxx.mongodb.net/?retryWrites=true&w=majority";
     const client = new MongoClient(url);
-    await client.connect();
+    await client.connect();ndbnbd
     db = client.db("CAR_RENT");
     cb();
 }
