7f399ac5969c46db26fa19669907346b6fd7563d
keerthanapen <keerthanapenugonda9@gmail.com>
2023-05-06 15:23:43 +0530

    ok

--- frontend/src/about.js
+++ frontend/src/about.js
@@ -10,11 +10,15 @@ function About(){
                 <div>
                     <img src="park2.webp" height="350px" width="100%"/>
                     <h1 className="about_name">ABOUT US</h1>
-
                 </div>
                 <div className="gap"></div>
                 <div>
-                    <p>havshygvfytfsty</p>
+                    <p>CARS19  car rental services are the market leaders in Ground Transportation since many years.
+                         Whether you want to go outstation for a weekend trip you can rent a car and drive it wherever
+                          you heart desires. CARS19  Car rent takes car of the hassle free car rental for you,
+                           whenever you need one. We have a wide range of luxury Cars and highend cars  for you.
+                           CARS19  Car rental is a car rental company that has gained popularity as Best Self Drive 
+                           Car Rental in AP.</p>
                 </div>
 
            </div>
